0984732ad134ea36a519018b75618a84616b5bba
Emil Rudeanu <emil.rudeanu@tellence.net>
2019-09-30 22:28:00 +0300

    changes some libraries

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-require('chromedriver');
 const webDriver = require('selenium-webdriver');
+
 const driver = new webDriver.Builder().forBrowser('chrome').build();
-const url = 'https://shadowantz.github.io/react_app/index.html';
 
 function searchForBugs() {
-	driver.get(url).then(() => {
+	driver.get('https://shadowantz.github.io/react_app/index.html').then(() => {
 		driver.findElement(webDriver.By.css('input .MuiInputBase-input'));
+		// TODO: will move to jest and react test only because selenium-webdriver doesn't want to run in a CI environment...
 	});
 }
 
